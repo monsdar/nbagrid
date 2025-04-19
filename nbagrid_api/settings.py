7821545c608f40e38694e9413e98ad720bf7d8c8
Nils Brinkmann <monsdar@gmail.com>
2025-04-19 13:01:46 +0200

    Added loading some settings from the deployment env

--- nbagrid_api/settings.py
+++ nbagrid_api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3%1y9=l0hq-(5+j&3nq-!8=_(3lb3uis*3@8miu0ucthzpv^-*'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-3%1y9=l0hq-(5+j&3nq-!8=_(3lb3uis*3@8miu0ucthzpv^-*')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False if 'DJANGO_PRODUCTION' in os.environ else True
 
 ALLOWED_HOSTS = ['nbagrid.pythonanywhere.com']
 
